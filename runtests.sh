b62c31e94292f0859599a77277260c73e3899f75
Gertjan Oude Lohuis <gertjan@byte.nl>
2013-01-15 11:38:49 +0100

    run tests every 2s, exit if virtualenv not found

--- runtests.sh
+++ runtests.sh
@@ -8,6 +8,7 @@ if [ -z "$VIRTUAL_ENV" ]; then
 		. ../bin/activate
 	else 
 		echo "Please activate your environment first!"
+		exit 1
 	fi
 fi
 
@@ -26,7 +27,7 @@ if [ "$1" = "--jenkins" ]; then
 	nosetests --with-xunit;
 	./pep8.sh
 else
-	watch -n 0.1 -c -- "
+	watch -n 2 -c -- "
 		find . -type f -name '*.pyc' -delete;
 		nosetests --with-yanc --yanc-color=on $*;
 		./pep8.sh"
