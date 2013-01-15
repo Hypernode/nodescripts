bc403ccbc96daf9b8c46aa4c3312f0949b8875ff
Allard Hoeve <allard@byte.nl>
2013-01-15 13:47:44 +0100

    Add setup.py, rename check_mailout.py to check_mailout

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='hypernode-system',
+    version='0.1',
+    packages=['hypernode.healthcheck'],
+    scripts=['bin/check_mailout'],
+    url='https://github.com/hypernode/nodescripts.git',
+    license='',
+    author='Allard Hoeve',
+    author_email='allardhoeve@gmail.com',
+    description=''
+)
